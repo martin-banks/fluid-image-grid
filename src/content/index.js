1cabbd0bea3c287fa0a9c04884faf16db275bc64
martin-banks <anotherbanksy@gmail.com>
2017-09-06 16:28:12 +1000

    add different image shapes

--- src/content/index.js
+++ src/content/index.js
@@ -1,31 +1,44 @@
-import * as test_4x3 from './images/test_4x3'
+import * as test4x3 from './images/test_4x3'
+import * as test3x4 from './images/test_3x4'
+import * as test16x9 from './images/test_16x9'
 
 const content = {
-	title: '',
-	intro: '',
+	title: 'Fluid grid title',
+	intro: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis repellendus nesciunt, quasi eligendi mollitia voluptatum adipisci minima ut temporibus doloremque totam ullam animi optio, non corporis, a maxime deserunt facere!',
 	parts: [
 		{
-			"image": test_4x3,
-			"caption": "Caption text including credit if required"
+			image: test4x3,
+			caption: 'Caption text including credit if required',
 		},
 		{
-			"image": test_4x3,
-			"caption": "Caption text including credit if required"
+			image: test16x9,
+			caption: 'Caption text including credit if required',
 		},
 		{
-			"image": test_4x3,
-			"caption": "Caption text including credit if required"
+			image: test3x4,
+			caption: 'Caption text including credit if required',
 		},
 		{
-			"image": test_4x3,
-			"caption": "Caption text including credit if required"
+			image: test16x9,
+			caption: 'Caption text including credit if required',
 		},
 		{
-			"image": test_4x3,
-			"caption": "Caption text including credit if required"
+			image: test16x9,
+			caption: 'Caption text including credit if required',
 		},
-
-	]
+		{
+			image: test3x4,
+			caption: 'Caption text including credit if required',
+		},
+		{
+			image: test4x3,
+			caption: 'Caption text including credit if required',
+		},
+		{
+			image: test3x4,
+			caption: 'Caption text including credit if required',
+		},
+	],
 }
 
 export default content
